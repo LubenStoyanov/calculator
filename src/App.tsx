f1bb5d24655d0633bb5decb84fe33b23eea8bfc3
Luben Stoyanov <luben.stoyanov.ls@gmail.com>
2022-12-15 18:27:42 +0100

    Refactor render of pad, refactor current number logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
 // Screen is still buggy after several calculations
 import { Center, Box, Grid, GridItem } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import "./calc.css";
+
+const buttons = [
+  { value: "AC", label: "AC" },
+  { value: "+/-", label: "plusminus" },
+  { value: "%", label: "precentage" },
+  { value: "÷", label: "/" },
+  { value: "7", label: "7" },
+  { value: "8", label: "8" },
+  { value: "9", label: "9" },
+  { value: "x", label: "x" },
+  { value: "4", label: "4" },
+  { value: "5", label: "5" },
+  { value: "6", label: "6" },
+  { value: "-", label: "-" },
+  { value: "1", label: "1" },
+  { value: "2", label: "2" },
+  { value: "3", label: "3" },
+  { value: "+", label: "+" },
+  { value: "0", label: "0" },
+  { value: ".", label: "," },
+  { value: "=", label: "=" },
+];
 
 function App() {
   const [screenValue, setScreenValue] = useState<null | string>("0");
@@ -10,13 +33,36 @@ function App() {
   const [showResult, setShowResult] = useState(true);
   const [isFirstDigit, setIsFirstDigit] = useState(true);
   const [isSecondNumber, setIsSecondNumber] = useState(false);
+  const [lastUserInput, setLastUserInput] = useState("");
+  const [showDelete, setShowDelete] = useState(false);
 
   const resetCalculator = () => {
     setResult("0");
     setNumber(null);
     setOperation(null);
+    setShowResult(true);
     setIsFirstDigit(true);
     setIsSecondNumber(false);
+    setShowDelete(false);
+  };
+
+  const calculate = (
+    number1: number,
+    number2: number,
+    operater: string | null
+  ) => {
+    switch (operater) {
+      case "+":
+        return number1 + number2;
+      case "-":
+        return number1 - number2;
+      case "x":
+        return number1 * number2;
+      case "÷":
+        return number1 / number2;
+      default:
+        return 0;
+    }
   };
 
   const calculateResult = (
@@ -24,75 +70,67 @@ function App() {
     operation: string | null,
     number: string | null
   ) => {
-    switch (operation) {
-      case "+":
-        return (Number(result) + Number(number)).toString();
-      case "-":
-        return (Number(result) - Number(number)).toString();
-      case "x":
-        return (Number(result) * Number(number)).toString();
-      case "/":
-        return (Number(result) / Number(number)).toString();
-      default:
-        return "";
-    }
+    const num1 = Number(result);
+    const num2 = Number(number);
+
+    return calculate(num1, num2, operation).toString();
   };
 
   const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    event.preventDefault();
     const value = event.currentTarget.textContent;
     if (!value) return;
 
+    setLastUserInput(!isNaN(+value) ? "number" : "operation");
+    setShowDelete(true);
+
+    if (value === "C") {
+      if (!isNaN(+value)) {
+      }
+    }
     if (!isNaN(+value)) {
-      if (isFirstDigit && !isSecondNumber) {
-        console.log("test");
-        setResult(value);
+      if (!isSecondNumber) {
+        isFirstDigit ? setResult(value) : setResult((v) => (v += value));
         setIsFirstDigit(false);
-      } else if (!isFirstDigit && !isSecondNumber) {
-        setResult((v) => (v += value));
-      } else if (isFirstDigit && isSecondNumber) {
+      } else {
+        isFirstDigit ? setNumber(value) : setNumber((v) => (v += value));
         setIsFirstDigit(false);
-        setNumber(value);
-      } else if (!isFirstDigit && isSecondNumber) {
-        setNumber((v) => (v += value));
       }
     } else if (value === "AC") {
       resetCalculator();
     } else {
       if (value === "=" && !number) return;
+
       setShowResult((sr) => !sr);
+
       if (value === "=" && operation) {
         setResult(calculateResult(result, operation, number));
         setShowResult(true);
         setNumber(null);
       }
-      setIsSecondNumber(true);
+
       setOperation(value);
-      setIsFirstDigit(true);
 
       if (number) {
-        console.log("calc");
+        setShowResult(true);
         setResult(calculateResult(result, operation, number));
-        // setIsSecondNumber(false);
       }
-    }
-  };
 
-  useEffect(() => {
-    // setScreenValue(isSecondNumber ? number : result);
+      setIsFirstDigit(true);
+      setIsSecondNumber(true);
+    }
+
     setScreenValue(showResult ? result : number);
-    console.log("useEffect", result, number, operation, isSecondNumber);
-  }, [result, number, operation]);
+  };
 
   return (
     <>
       <Grid
         templateAreas={`"screen screen screen screen"
-                        "ac plusminus precentage divide"
-                        "seven eight nine multiply"
-                        "four five six minus"
-                        "one two three plus"
-                        "zero zero comma equal"`}
+                      "ac plusminus precentage divide"
+                      "seven eight nine multiply"
+                      "four five six minus"
+                      "one two three plus"
+                      "zero zero comma equal"`}
         gap="1"
         m="3rem"
         fontSize="1.5rem"
@@ -100,223 +138,28 @@ function App() {
       >
         <GridItem area={"screen"}>
           <Box h="10" borderRadius="50" pr="1" border="1px" textAlign={"right"}>
+            {/* {showResult ? result : number} */}
             {screenValue}
-            {/* {isSecondNumber ? number : result} */}
           </Box>
         </GridItem>
-        <GridItem>
-          <Center
+        {buttons.map((button) => (
+          <GridItem
+            key={button.label}
+            area={button.label === "0" ? "zero" : ""}
             onClick={handleClick}
-            boxSize="10"
-            borderRadius="50"
-            border="1px"
-            id="AC"
           >
-            AC
-          </Center>
-        </GridItem>
-        <GridItem>
-          <Center
-            onClick={handleClick}
-            boxSize="10"
-            borderRadius="50"
-            border="1px"
-            id="plusminus"
-            bg={"red"}
-          >
-            +/-
-          </Center>
-        </GridItem>
-        <GridItem>
-          <Center
-            onClick={handleClick}
-            boxSize="10"
-            borderRadius="50"
-            border="1px"
-            id="percentage"
-            bg={"red"}
-          >
-            %
-          </Center>
-        </GridItem>
-        <GridItem>
-          <Center
-            onClick={handleClick}
-            boxSize="10"
-            borderRadius="50"
-            border="1px"
-            id="AC"
-          >
-            /
-          </Center>
-        </GridItem>
-        <GridItem>
-          <Center
-            onClick={handleClick}
-            boxSize="10"
-            borderRadius="50"
-            border="1px"
-            id="7"
-          >
-            7
-          </Center>
-        </GridItem>
-        <GridItem>
-          <Center
-            onClick={handleClick}
-            boxSize="10"
-            borderRadius="50"
-            border="1px"
-            id="8"
-          >
-            8
-          </Center>
-        </GridItem>
-        <GridItem>
-          <Center
-            onClick={handleClick}
-            boxSize="10"
-            borderRadius="50"
-            border="1px"
-            id=""
-          >
-            9
-          </Center>
-        </GridItem>
-        <GridItem>
-          <Center
-            onClick={handleClick}
-            boxSize="10"
-            borderRadius="50"
-            border="1px"
-            id="AC"
-          >
-            x
-          </Center>
-        </GridItem>
-        <GridItem>
-          <Center
-            onClick={handleClick}
-            boxSize="10"
-            borderRadius="50"
-            border="1px"
-            id="AC"
-          >
-            4
-          </Center>
-        </GridItem>
-        <GridItem>
-          <Center
-            onClick={handleClick}
-            boxSize="10"
-            borderRadius="50"
-            border="1px"
-            id="AC"
-          >
-            5
-          </Center>
-        </GridItem>
-        <GridItem>
-          <Center
-            onClick={handleClick}
-            boxSize="10"
-            borderRadius="50"
-            border="1px"
-            id="AC"
-          >
-            6
-          </Center>
-        </GridItem>
-        <GridItem>
-          <Center
-            onClick={handleClick}
-            boxSize="10"
-            borderRadius="50"
-            border="1px"
-            id="minus"
-          >
-            -
-          </Center>
-        </GridItem>
-        <GridItem>
-          <Center
-            onClick={handleClick}
-            boxSize="10"
-            borderRadius="50"
-            border="1px"
-            id="AC"
-          >
-            1
-          </Center>
-        </GridItem>
-        <GridItem>
-          <Center
-            onClick={handleClick}
-            boxSize="10"
-            borderRadius="50"
-            border="1px"
-            id="AC"
-          >
-            2
-          </Center>
-        </GridItem>
-        <GridItem>
-          <Center
-            onClick={handleClick}
-            boxSize="10"
-            borderRadius="50"
-            border="1px"
-            id="AC"
-          >
-            3
-          </Center>
-        </GridItem>
-        <GridItem>
-          <Center
-            onClick={handleClick}
-            boxSize="10"
-            borderRadius="50"
-            border="1px"
-            id="plus"
-          >
-            +
-          </Center>
-        </GridItem>
-        <GridItem area={"zero"}>
-          <Box
-            onClick={handleClick}
-            h="10"
-            borderRadius="50"
-            pl="1"
-            border="1px"
-            id="AC"
-          >
-            0
-          </Box>
-        </GridItem>
-        <GridItem>
-          <Center
-            onClick={handleClick}
-            boxSize="10"
-            borderRadius="50"
-            border="1px"
-            id="AC"
-            bg={"red"}
-          >
-            ,
-          </Center>
-        </GridItem>
-        <GridItem>
-          <Center
-            onClick={handleClick}
-            boxSize="10"
-            borderRadius="50"
-            border="1px"
-            id="equal"
-          >
-            =
-          </Center>
-        </GridItem>
+            <Center
+              boxSize="20"
+              borderRadius="50"
+              border="1px"
+              fontWeight="bold"
+              cursor="pointer"
+            >
+              {/* {showDelete && button.label === "AC" ? "C" : button.value} */}
+              {button.value}
+            </Center>
+          </GridItem>
+        ))}
       </Grid>
     </>
   );
